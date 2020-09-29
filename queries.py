fe5a25ed215fd6ec260aee6820ee88e670de5dec
Fredpwol <cyborg290@gmail.com>
2020-09-29 21:37:24 +0100

    added uuid to users

--- queries.py
+++ queries.py
@@ -1,19 +1,21 @@
 import sqlite3
-import sys
+import uuid
+
+FILENAME = "todo_cli.db"
 
 def create_db():
     db = sqlite3.connect(FILENAME)
     curr = db.cursor()
-    curr.execute("CREATE TABLE users (id INT PRIMARY KEY , username VARCHAR(50) NOT NULL, password VARCHAR(16) NOT NULL)")
-    curr.execute("CREATE TABLE task (id INT PRIMARY KEY, task VARCHAR(100) NOT NULL, status BOOLEAN DEFAULT 0 NOT NULL )")
+    curr.execute("CREATE TABLE users (id VARCHAR(32), username VARCHAR(50) NOT NULL, password VARCHAR(16) NOT NULL)")
+    curr.execute("CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, task VARCHAR(100) NOT NULL, status BOOLEAN DEFAULT 0 NOT NULL )")
     db.commit()
     curr.close()
     return db
 
 
-def get_user(db, id):
+def get_user(db, username, password):
     curr = db.cursor()
-    user = curr.execute("SELECT username, password FROM users WHERE ID=?",(id,))
+    user = curr.execute("SELECT username, password FROM users WHERE username=? AND password=?",(username, password))
     db.commit()
     curr.close()
     
@@ -23,13 +25,14 @@ def get_user(db, id):
 
 def create_user(db, username, password):
     curr = db.cursor()
-    curr.execute("INSERT INTO users VALUES (NULL,?,?)",(username, password))
+    uid = uuid.uuid4().hex
+    curr.execute("INSERT INTO users VALUES (?,?,?)",(uid,username, password))
     curr.close()
     db.commit()
 
 def list_users(db):
     curr = db.cursor()
-    users = curr.execute("SELECT * from users")
+    users = curr.execute("SELECT username from users")
     for user in users:
-        sys.stdout.write(users)
+        print(user[0])
     curr.close()
