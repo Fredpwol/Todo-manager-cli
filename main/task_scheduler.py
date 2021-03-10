6fb8778be2c21ce18dd08662629b267fdaac1a94
Fredpwol <cyborg290@gmail.com>
2021-03-10 11:56:28 +0100

    used thread pools

--- main/task_scheduler.py
+++ main/task_scheduler.py
@@ -6,8 +6,9 @@ import queries
 import datetime
 import subprocess
 import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
 
-event_scheduler = sched.scheduler(time.time, time.sleep)
+
 
 
 def read_code(cmd):
@@ -20,15 +21,20 @@ def read_code(cmd):
     print(msg)
 
 queue = []
+
+def add_task(task):
+    event_scheduler = sched.scheduler(time.time, time.sleep)
+    if not task[0] in queue:
+        if time.time() <= task[3]:
+            event_scheduler.enterabs(task[3], 1, read_code, argument=(task[1],))
+            queue.append(task[0])
+
+            event_scheduler.run(blocking=True)
+            
 def run_scheduler():
     commands = queries.get_commands()
-    for cmd in commands:
-        if not cmd[0] in queue:
-            if time.time() <= cmd[3]:
-                event_scheduler.enterabs(cmd[3], 1, read_code, argument=(cmd[1],))
-                queue.append(cmd[0])
-                # if (e in event_scheduler.queue): event_scheduler.cancel(e)
-    event_scheduler.run(blocking=True)
+    with ThreadPoolExecutor() as pool:
+        pool.map(add_task, commands)
 
 while True:
     p = multiprocessing.Process(target=run_scheduler)
